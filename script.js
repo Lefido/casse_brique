f3f5d7658fd942ea6960a6132b7a473f14c3d1c3
Lefido <lefido@hotmail.com>
2023-03-20 12:39:44 +0100

    mod resolution

--- script.js
+++ script.js
@@ -5,35 +5,38 @@ import { Balle } from "./js/Balle.js"
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-canvas.width = 700 // window.innerWidth
+canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 const game = new Game(canvas.width, canvas.height)
 
 let canvasPosition = canvas.getBoundingClientRect()
 
-window.addEventListener('resize', () => {
-    canvas.width = 700 // window.innerWidth
-    canvas.height = window.innerHeight
-   
-})
 
-window.addEventListener('mousedown', (e) => {
-
-
-    let nbBall = 1
-    for (let i=0; i<nbBall; i++){
-        game.addBalle(e.x - canvasPosition.left, game.player.y - 10)
-    }
-    
-})
-
-window.addEventListener('mousemove', (e)=> {
-    
-    game.player.update(e.x - canvasPosition.left)
-})
 
 window.addEventListener('load', ()=> {
+
+    window.addEventListener('resize', () => {
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+        this.game.width = canvas.width
+        this.game.height = canvas.height
+       
+    })
+    
+    window.addEventListener('mousedown', (e) => {
+    
+        let nbBall = 1
+        for (let i=0; i<nbBall; i++){
+            game.addBalle(e.x - canvasPosition.left, game.player.y - 10)
+        }
+        
+    })
+    
+    window.addEventListener('mousemove', (e)=> {
+        
+        game.player.update(e.x - canvasPosition.left)
+    })
 
     function animate() {
 
